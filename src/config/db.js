cd6594382ffd6645ab74fc447d878ad5e883d457
MANUEL SANCHEZ MAGUIÑA <91157092+msanc12@users.noreply.github.com>
2025-03-05 20:29:15 -0500

    Help

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB conectado');
-  } catch (error) {
-    console.error('MongoDB error:', error);
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.error(err.message);
     process.exit(1);
   }
 };
